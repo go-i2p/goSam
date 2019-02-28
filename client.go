5ffafcd28dddec80aff7dce63f9064f0491b3532
idk <problemsolver@openmailbox.org>
2019-02-27 22:00:35 -0500

    explicitly et invalid initial lastaddr

--- client.go
+++ client.go
@@ -96,6 +96,7 @@ func NewClientFromOptions(opts ...func(*Client) error) (*Client, error) {
 	c.debug = false
 	c.sigType = SAMsigTypes[4]
 	c.id = 0
+	c.lastaddr = "invalid"
 	for _, o := range opts {
 		if err := o(&c); err != nil {
 			return nil, err
