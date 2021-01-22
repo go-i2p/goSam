f97683379f2365e373e37fed5dd986740c973fd1
idk <hankhill19580@gmail.com>
2021-01-22 16:18:17 -0500

    make a version which stringifies i2pkeys

--- i2pkeys.go
+++ i2pkeys.go
@@ -26,7 +26,7 @@ func (c *Client) NewDestination(sigType ...string) (string, string, error) {
 		return "", "", errors.New("failed to generate private destination key")
 	}
 	if pub = r.Pairs["PUB"]; pub == "" {
-		return priv, "",  errors.New("failed to generate public destination key")
+		return priv, "", errors.New("failed to generate public destination key")
 	}
 	return priv, pub, nil
 }
